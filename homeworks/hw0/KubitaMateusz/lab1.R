a69b049611f418f92df544014bd1ce8006b5de95
Koliber216 <100526262+Koliber216@users.noreply.github.com>
2022-10-05 11:36:34 +0200

    [hw0] Kubita Mateusz

--- homeworks/hw0/KubitaMateusz/lab1.R
+++ homeworks/hw0/KubitaMateusz/lab1.R
@@ -30,12 +30,27 @@ head(mtcars)
 
 ?mtcars
 
-print("321321321")
+
+dim(mtcars)
+nrow(mtcars)
+ncol(mtcars)
+
+mtcars[,3]
+typeof(mtcars)
+str(mtcars)
+mtcars[2:3,4:5]
+mtcars[c(2,3), c(4,5)]
+mtcars[1:10,c(2,3)]
+mtcars[,"mpg"]
+
+mtcars[,c("mpg","disp")]
+mtcars[c("disp", "mpg")]
 
 
 # Jak wybieramy wiersze (obserwacje) oraz kolumny (zmienne)?
 
 # Pierwszy wiersz, pierwsza kolumna?
+
 
 # 10 pierszych wierszy, 2 i 3 kolumna?
 
@@ -43,29 +58,77 @@ print("321321321")
 
 # Wszystkie wiersze i kolumny w kolejności "am", "wt", "mpg"?
 
+mtcars[,c("am", "wt", "mpg")]
+
 # Jak wybierać jedną kolumnę?
 
+mtcars$mpg
+
 # Uwaga na przecinek i wybór kolumn poprzez indeksy
+mtcars[1,2]
+mtcars[c(1,2)]
+
+mean(mtcars[,1])
+
+str(mtcars)
 
 # Pytania
 
 # 1. Wymiar ramki danych
 
+dim(mtcars)
+
 # 2. Jakie są typy zmiennych?
+
+str(mtcars)
 
 # 3. Ile jest unikalnych wartości zmiennej "cyl" i jakie to są wartości?
 
+unique(mtcars$cyl)
+length(unique(mtcars$cyl))
+
+
 # 4. Jaka jest średnia wartość zmiennej "drat" dla samochodów o wartości zmiennej "cyl" równej 4?
+mtcars$cyl == 4
+mtcars[mtcars$cyl == 4,]
+mean(mtcars[mtcars$cyl == 4,]$drat)
+median(mtcars[mtcars$cyl == 4,]$dra)
+mtcars$drat
 
 # 5. Jakie są unikalne wartości zmiennej "am" i jaki jest ich rozkład (liczba wystąpień)? 
 
+unique(mtcars$am)
+table(mtcars$am)
+
 # Prosty wykres
 
-# Zależność "mpg" i "hp" - scatter plot
 
+# Zależność "mpg" i "hp" - scatter plot
+plot(mtcars$mpg, mtcars$hp)
 
 
 # Zmienna "cyl" - barplot
+
+barplot(table(mtcars$cyl))
+
+
+# wykres kolowy
+pie(table(mtcars$cyl))
+
+
+# histogram
+hist(mtcars$mpg)
+?hist
+hist(mtcars$mpg, breaks = 10)
+
+#box plot
+boxplot(mtcars$mpg)
+
+max(mtcars$mpg)
+min(mtcars$mpg)
+
+boxplot(mtcars$hp, mtcars$mpg)
+
 
 
 ## 4) Gra proton, należy stworzyć plik R z kodami do rozwiązania gry (do 20 minut).
@@ -74,5 +137,50 @@ install.packages("proton")
 library(proton)
 proton()
 
+employees
 
+dim(employees)
+head(employees)
+employees[employees$surname == "Pietraszko",]
+
+employees[employees$surname == "Insecure",]
+
+proton(action = "login", login = "johnins")
+
+head(top1000passwords)
+dim(top1000passwords)
+typeof(top1000passwords)
+length(unique(top1000passwords))
+top1000passwords[1]
+head(top1000passwords)
+
+
+proton(action = "login", login = "johnins", password = "qwerty")
+
+for (pass in 1:1000) {
+  proton(action = "login", login = "johnins", password = top1000passwords[pass])
+}
+
+head(logs)
+
+proton()
+head(employees)
+employees[employees$surname == "Pietraszko",]
+
+head(logs)
+logs[logs$login == "slap",]
+unique(logs[logs$login == "slap",]$host)
+table(logs[logs$login == "slap",]$host)
+max(table(logs[logs$login == "slap",]$host))
+which.max(table(logs[logs$login == "slap",]$host))
+
+proton(action = "server", host="194.29.178.16")
+
+head(bash_history)
+length(bash_history)
+typeof(bash_history)
+
+
+
+#194.29.178.16 
 ## 5) Umieszczamy rozwiązanie na repozytorium.
